e20f3286986aebf5795473b694f4d963ad7851b2
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-04-13 23:56:39 -0400

    added skills and edu

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faGolang, faReact, faDocker, faJs } from '@fortawesome/free-brands-svg-icons'
+
+type EduProps = {
+  institution : string
+  name : string
+  extraInfo : string | null
+}
+
+function Edu(props : EduProps) : JSX.Element{
+  let imageURI : string = props.institution === "Platzi" ? "../platzi.jpeg" : "../mdc.jpeg"
+  return(
+    <div className='edu'>
+      <div className='logo'>
+        <img src={imageURI} height="30%" width="100%"/>
+      </div>
+      <div className='edu-info'>
+        <h4>{props.institution}</h4>
+        <h5>{props.name}</h5>
+        {props.extraInfo ? <span className='minimal-info'>{props.extraInfo}</span> : null}
+      </div>
+    </div>
+  )
+}
 
 function App()  {
 
@@ -44,19 +67,45 @@ function App()  {
         </div>
 
         <div id="college">
-          <div id="mdc">
-            <img width="100%" height="45%" src='../mdc.jpeg'/>
+          <Edu institution='Miami Dade College'
+          name='Associates in Arts, Computer Science'
+          extraInfo='2018-present' />
+          <Edu institution='Platzi'
+          name='Algorithmic Complexity with Javascript'
+          extraInfo="2022"/>
+          <Edu institution='Platzi'
+          name='Data Structures with Javascript'
+          extraInfo='2022'/>
+          <Edu institution='Platzi'
+          name="Go Advanced: Event Driven Architecture and CQRS"
+          extraInfo="2022"/>
+          <Edu institution='Platzi'
+          name='Go Advanced: Concurrency and Design Patterns'
+          extraInfo='2022'/>
+          <Edu institution='Platzi'
+          name='Go Advanced: Rest and Websockets'
+          extraInfo='2022'/>
+        </div>
+
+        <div id="skills">
+          <div id="skills-header">
+            <h2>Skills</h2>
           </div>
 
-          <div id="degree">
-            <h4>Miami Dade College</h4>
-            <span className='degree'>Associates of Arts, Computer Science</span>
-            <span className='degree'>may 2018-present</span>
+          <div id="skills-list">
+            <FontAwesomeIcon icon={faReact} size='1x'/>
+            <span>React</span>
+            <span>Ts</span>
+            <span>Typescript</span>
+            <FontAwesomeIcon icon={faJs} size='1x'/>
+            <span>Javascript</span>
+            <FontAwesomeIcon icon={faGolang} size='1x'/>
+            <span>Golang</span>
+            <FontAwesomeIcon icon={faDocker} size='1x'/>
+            <span>Docker</span>
           </div>
-
         </div>
       </div>
-
     </div>
   )
 }
