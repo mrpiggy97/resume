abce895bd4b3a581701dd8e62a7aaf8306811ec7
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-05-15 00:12:52 -0400

    added new frontend project to resume, added new components to better display projects

--- src/App.tsx
+++ src/App.tsx
@@ -27,33 +27,112 @@ function Edu(props : EduProps) : JSX.Element{
   )
 }
 
-type ProjectProps = {
+type FrontendProps = {
   name : string
   description : string
   link : string
-  imageURI : string | null
+  githubUrl : string
+  imageURI : string
 }
 
-function Project(props : ProjectProps) : JSX.Element{
+function Frontend(props : FrontendProps) : JSX.Element{
   return(
     <div className='proyect'>
       <div className='proyect-name'>
         <h4>{props.name}</h4>
       </div>
       <div className="proyect-link">
-        <a className='social' href={props.link} target="_blank">click here to see {props.name}</a>
+        <a className="social" href={props.link} target='_target'>click here to see {props.name}</a>
+        <a className="social" href={props.githubUrl} target="_blank">github repo</a>
       </div>
       <div className='proyect-description'>
         <span>{props.description}</span>
       </div>
       <div className='proyect-image'>
-        {props.imageURI ? <img src={props.imageURI} width="100%" height="100%" /> : null}
+        <img src={props.imageURI} width="100%" height="100%" />
+      </div>
+    </div>
+  )
+}
+
+type BackendProps = {
+  name : string,
+  description : string,
+  githubUrl : string
+}
+
+function Backend(props : BackendProps) : JSX.Element{
+  return(
+    <div className='proyect'>
+      <div className='proyect-name'>
+        <h4>{props.name}</h4>
+      </div>
+      <div className="proyect-link">
+        <a className="social" href={props.githubUrl} target="_blank">github repo</a>
+      </div>
+      <div className='proyect-description'>
+        <span>{props.description}</span>
+      </div>
+    </div>
+  )
+}
+
+type Service = {
+  name : string,
+  link : string
+  imageUrl : string | null
+}
+
+type MicroServicesProps = {
+  name : string,
+  description : string,
+  services : Service[]
+}
+
+function MicroServices(props : MicroServicesProps) : JSX.Element{
+  let servicesWithImage : Service[] = props.services.filter((service, index) => {
+    if(service.imageUrl){
+      return service
+    }
+  })
+  let imageUrl : string = ""
+  if(servicesWithImage[0].imageUrl !== null){
+    imageUrl = servicesWithImage[0].imageUrl
+  }
+
+  return(
+    <div className='proyect'>
+      <div className='proyect-name'>
+        <h4>{props.name}</h4>
+      </div>
+      <div className="proyect-link">
+        {props.services.map((service) => {
+        return <a className='social' href={service.link} target="_blank">click here to see {service.name}</a>
+        })}
+      </div>
+      <div className='proyect-description'>
+        <span>{props.description}</span>
+      </div>
+      <div className='proyect-image'>
+        {imageUrl ? <img src={imageUrl} width="100%" height="100%" /> : null}
       </div>
     </div>
   )
 }
 
 function App()  {
+
+  let restBackend : Service = {
+    name: "rest backend",
+    link: "https://www.github.com/mrpiggy97/rest",
+    imageUrl : null,
+  }
+  let restFrontend : Service = {
+    name : "rest frontend",
+    link: "https://www.github.com/mrpiggy97/restClient",
+    imageUrl: "/images/restClient.gif",
+  }
+  let microservices : Service[] = [restBackend,restFrontend]
 
   return (
     <div id="app">
@@ -86,38 +165,58 @@ function App()  {
 
       <div id="proyects">
         <h2>Projects</h2>
-        <Project
+        <Frontend
         name="mypathfinder"
         link="https://boisterous-cocada-9ea93b.netlify.app/"
         description="mypathfinder was built with React.js and Typescript,
                     its objective was to make a project that implements Dijkstra's
                     algorithm with animations that show you how the graph grows to
                     find the end node. It was deployed using netlify"
+        githubUrl='https://www.github.com/mrpiggy97/mypathfinder'
         imageURI="/images/mypathfinder.gif"/>
-        <Project
-        name='restClient'
-        link='https://github.com/mrpiggy97/restClient'
-        description="restClient is a chat app built in React.js and Typescript
-                    that uses WebSockets and rest's proyect api to connect users"
-        imageURI="/images/restClient.gif"
+        <Frontend
+        name='space simulation'
+        link='https://zingy-hotteok-ae1f96.netlify.app/'
+        description="a 3d simulation of the solar sisytem, it was built using
+                    React.js, Typescript and Three.js"
+        githubUrl='https://www.github.com/mrpiggy97/space-simulation'
+        imageURI="/images/space-simulation.gif"
         />
-        <Project
+        <MicroServices
         name='rest'
-        link='https://github.com/mrpiggy97/rest'
-        description='rest is an api built with Go that serves restClient proyect,
-                    it receives messages and sends them to users through WebSockets'
-        imageURI={null}
+        services={microservices}
+        description='a collection of microservies, rest was built with go, it receives
+                    messages and sends them to users through websockets, rest client
+                    was built using React.js and typescript and it connects users with backend'
         />
-        <Project
+        <Backend
         name='cqrs'
-        link='https://github.com/mrpiggy97/cqrs'
         description='cqrs is a proyect built in Go that implements cqrs and elastic search,
                     its objective was to build services based on event driven architecture'
-        imageURI={null}
+        githubUrl='https://www.github.com/mrpiggy97/cqrs'
         />
       </div>
 
       <div id="education">
+      <div id="skills">
+          <div id="skills-header">
+            <h2>Skills</h2>
+          </div>
+
+          <div id="skills-list">
+            <FontAwesomeIcon icon={faReact} size='1x'/>
+            <span>React</span>
+            <span>Ts</span>
+            <span>Typescript</span>
+            <FontAwesomeIcon icon={faJs} size='1x'/>
+            <span>Javascript</span>
+            <FontAwesomeIcon icon={faGolang} size='1x'/>
+            <span>Go</span>
+            <FontAwesomeIcon icon={faDocker} size='1x'/>
+            <span>Docker</span>
+          </div>
+        </div>
+
         <div id="college-header">
           <h2>Education</h2>
         </div>
@@ -141,25 +240,6 @@ function App()  {
           <Edu institution='Platzi'
           name='Go Advanced: Rest and Websockets'
           extraInfo='2022'/>
-        </div>
-
-        <div id="skills">
-          <div id="skills-header">
-            <h2>Skills</h2>
-          </div>
-
-          <div id="skills-list">
-            <FontAwesomeIcon icon={faReact} size='1x'/>
-            <span>React</span>
-            <span>Ts</span>
-            <span>Typescript</span>
-            <FontAwesomeIcon icon={faJs} size='1x'/>
-            <span>Javascript</span>
-            <FontAwesomeIcon icon={faGolang} size='1x'/>
-            <span>Go</span>
-            <FontAwesomeIcon icon={faDocker} size='1x'/>
-            <span>Docker</span>
-          </div>
         </div>
       </div>
     </div>
